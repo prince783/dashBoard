30808319c393f5d6ee2063f985f185a9f21c9783
prince783 <princeojha783@gmail.com>
2025-06-10 12:45:50 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
+  base: "/https://github.com/prince783/dashBoard.git",
   plugins: [
     tailwindcss(),
   ],
